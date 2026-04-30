aecf23885d98bd085bdc67edf7eb6b3f79872560
Christian F. Badillo H. <cris.badillo1408@gmail.com>
2026-04-29 21:33:07 -0600

    feat(backend/envs): Add wrappers for Discrete and Continuous RL Env and refactor some code in Maze Env

---
+++ backend/envs/gridworld/mazes.py
@@ -0,0 +1,165 @@
+from typing import Any, Dict, Tuple
+import gymnasium as gym
+import numpy as np
+from ...core.env_interfaces import AbstractDiscreteEnv
+from _mazes_types import (
+    MazeType,
+    MAZE_FOUR_ROOMS,
+    MAZE_OPEN,
+    MAZE_U_SHAPE,
+    MAZE_WALL,
+    MAZE_ZIGZAG,
+)
+
+mazes = {
+    MazeType.OPEN: MAZE_OPEN,
+    MazeType.ROOMS: MAZE_FOUR_ROOMS,
+    MazeType.USHAPE: MAZE_U_SHAPE,
+    MazeType.WALL: MAZE_WALL,
+    MazeType.ZIGZAG: MAZE_ZIGZAG,
+}
+
+
+class MazeEnv(AbstractDiscreteEnv):
+    """
+    Custom Maze Environment
+    """
+
+    def __init__(self, type: MazeType, max_steps: int = 100) -> None:
+        # Maze Information
+        self.type = type
+        self.max_steps = max_steps
+        self.maze = self._parse_maze(mazes[type])
+
+        # Agent information
+        # - Get the position of the agent Spawn with a np.ndarray
+        self._agent_position = np.array([-1, -1], dtype=np.int8)
+        self._target_position = np.array([-1, -1], dtype=np.int8)
+
+        # Maze shape
+        self.rows, self.cols = self.maze.shape
+
+        # Observation Space
+        self.observation_space = gym.spaces.Discrete(self.rows * self.cols)
+
+        # Actions
+        self.action_space = gym.spaces.Discrete(4)
+        self._action_to_direction = {
+            0: np.array([0, 1]),  # Right
+            1: np.array([-1, 0]),  # Up
+            2: np.array([0, -1]),  # Left
+            3: np.array([1, 0]),  # Down
+        }
+
+        # Env Step info
+        self.reward = 0.0
+        self.terminated = False
+        self.truncated = False
+        self.steps = 0
+
+    def step(self, action: int) -> tuple[int, float, bool, bool, dict]:
+        """
+        Execute the action over the maze
+        """
+
+        self.steps += 1
+
+        if self.steps == self.max_steps:
+            self.truncated = True
+
+        # Map action to direction
+        direction = self._action_to_direction[action]
+
+        # Proposed new state
+        new_position = self._agent_position + direction
+        bounderies_mask = (
+            (new_position[0] < 0)
+            | (new_position[0] > 9)
+            | (new_position[1] < 0)
+            | (new_position[1] > 9)
+        )
+        if bounderies_mask:
+            out_bounderies = True
+            is_free = False
+        else:
+            out_bounderies = False
+            is_free = self.maze[new_position[0], new_position[1]] != 1
+
+        # Checking bounderies
+        if bounderies_mask or not is_free:
+            out_bounderies = True
+            self.reward = -2.0
+            observation = self._get_obs()
+            info = self._get_info()
+            info["out_boundaries"] = out_bounderies
+            self.terminated = False
+
+            return observation, self.reward, self.terminated, self.truncated, info
+
+        self._agent_position += direction
+        out_bounderies = False
+        observation = self._get_obs()
+
+        self.terminated = np.array_equal(self._agent_position, self._target_position)
+        self.reward = 10 if self.terminated else -1.0
+
+        info = self._get_info()
+        info["out_boundaries"] = out_bounderies
+
+        return observation, self.reward, self.terminated, self.truncated, info
+
+    def reset(
+        self, *, seed: int | None = None, options: dict[str, Any] | None = None
+    ) -> Tuple[int, dict[str, Any]]:
+        """
+        Reset the environment for each maze type.
+        """
+        np.random.seed(seed)
+
+        self._agent_position = np.argwhere(self.maze == 2)[0]
+        self._target_position = np.argwhere(self.maze == 3)[0]
+
+        observation = self._get_obs()
+        info = self._get_info()
+        # Env Step info
+        self.reward = 0.0
+        self.terminated = False
+        self.truncated = False
+        self.steps = 0
+
+        return observation, info
+
+    @property
+    def maze_shape(self) -> tuple[int, int]:
+        return self.maze.shape
+
+    def _get_info(self) -> Dict[str, Any]:
+        """Compute auxiliary information for debugging.
+
+        Returns:
+            dict: Info with distance between agent and target
+        """
+        return {
+            "distance": np.linalg.norm(
+                self._agent_position - self._target_position, ord=1
+            )
+        }
+
+    def _get_obs(self) -> int:
+        row, col = self._agent_position
+
+        return self._encode_state(row, col)
+
+    def _parse_maze(self, maze_layout: list[str]) -> np.ndarray:
+        """
+        Map maze text to matrix with using the standar:
+        0: Free (.), 1: Obstacule (#), 2: Spawn (S), 3: Goal (G)
+        """
+        mapping = {".": 0, "#": 1, "S": 2, "G": 3}
+        matrix = np.zeros((10, 10), dtype=np.int8)
+
+        for i, row in enumerate(maze_layout):
+            for j, char in enumerate(row):
+                matrix[i, j] = mapping[char]
+
+        return matrix
---
+++ backend/envs/wrappers.py
@@ -0,0 +1,40 @@
+from ..core.wrappers import BaseUIWrapper
+from ..core.env_interfaces import AbstractDiscreteEnv
+from ..types.data import EnvStep
+
+
+class DiscreteEnvUIWrapper(BaseUIWrapper):
+    def __init__(self, env: AbstractDiscreteEnv) -> None:
+        self.env = env
+
+    def reset(self, seed: int | None = None) -> tuple[int, EnvStep]:
+        """Retorna (estado_puro_para_agente, datos_visuales_para_ui)"""
+        obs_int, info = self.env.reset(seed=seed)
+        row, col = self.env._decode_state(obs_int)
+
+        ui_state = {"render_type": "GRID_INIT", "agent_pos": [row, col]}
+        ui_step = EnvStep(
+            action=-99,
+            state=ui_state,
+            reward=0.0,
+            terminated=False,
+            truncated=False,
+            info=info,
+        )
+        return obs_int, ui_step
+
+    def step(self, action: int | float | list) -> tuple[int, EnvStep]:
+        """Retorna (estado_puro_para_agente, datos_visuales_para_ui)"""
+        obs_int, reward, term, trunc, info = self.env.step(action)
+        row, col = self.env._decode_state(obs_int)
+
+        ui_state = {"render_type": "GRID_UPDATE", "agent_pos": [row, col]}
+        ui_step = EnvStep(
+            action=action,
+            state=ui_state,
+            reward=float(reward),
+            terminated=term,
+            truncated=trunc,
+            info=info,
+        )
+        return obs_int, ui_step
